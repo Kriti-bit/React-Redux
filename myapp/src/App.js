317974c2069a2030fd2bf705d182171e163f3a51
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-05-22 00:31:09 +0530

    #14 Outputting Lists

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import Ninjas from './Ninjas'
 
-function App() {
+class App extends Component {
+  state = {
+    ninjas : [
+      {name :'abc' , age : 25 , belt : 'black' , id:1},
+      {name :'def' , age : 30 , belt : 'green' , id:2},
+      {name :'ghi' , age : 35 , belt : 'purple' , id:3}
+    ]
+  }
+  render () {
   return (
     <div className="App">
               <h1>My First React App</h1>
               <p>Welcome</p>
-              <Ninjas name="abc" age="25" belt="black"/>
-              <Ninjas name="def" age="30" belt="green"/>
+              <Ninjas ninjas = { this.state.ninjas } />
     </div>
   );
+}
 }
 
 export default App;
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -4,12 +4,20 @@ class Ninjas extends Component {
     render(){
         //console.log(this.props)
         //Destructuring the props
-        const { name , age , belt } = this.props;
+        //const { name , age , belt } = this.props;
+        const { ninjas } = this.props;
+        const ninjaList = ninjas.map(ninja =>{
+            return (
+                <div className= "ninja" key = {ninja.id }>
+                    <div> { ninja.name } </div>
+                    <div> {ninja.age } </div>
+                    <div> {ninja.belt } </div>
+                </div>
+            )
+        })
         return(
-            <div className="ninja">
-                <div>Name: {name}</div> 
-                <div>Age: {age}</div>
-                <div>Belt: {belt}</div>
+            <div className="ninja-list">
+                { ninjaList }
             </div>
         );
         //Instead of using {name}, we can use {this.props.name} if we don't want to destructure the property
