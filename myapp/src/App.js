489702bbb20e6d90ee4e01035a7417d0216c6a13
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-05-21 23:59:38 +0530

    #13 Props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
               <h1>My First React App</h1>
               <p>Welcome</p>
-              <Ninjas/>
+              <Ninjas name="abc" age="25" belt="black"/>
+              <Ninjas name="def" age="30" belt="green"/>
     </div>
   );
 }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 
 class Ninjas extends Component {
     render(){
+        //console.log(this.props)
+        //Destructuring the props
+        const { name , age , belt } = this.props;
         return(
             <div className="ninja">
-                <div>Name: Ryu</div>
-                <div>Age: 30</div>
-                <div>Belt: Black</div>
+                <div>Name: {name}</div> 
+                <div>Age: {age}</div>
+                <div>Belt: {belt}</div>
             </div>
         );
+        //Instead of using {name}, we can use {this.props.name} if we don't want to destructure the property
     }
 }
 
