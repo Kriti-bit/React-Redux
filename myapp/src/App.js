56aeeffdb24408e5bc3cd157dae119917714a39f
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-05-24 23:52:46 +0530

    #18 Functions as props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -10,13 +10,20 @@ class App extends Component {
       {name :'ghi' , age : 35 , belt : 'purple' , id:3}
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas , ninja]; //If we useninjas: this.state.ninjas.push(ninja) the elements stored in the array previously get deleted
+    this.setState({
+      ninjas : ninjas
+    })
+  }
   render () {
   return (
     <div className="App">
               <h1>My First React App</h1>
               <p>Welcome</p>
               <Ninjas ninjas = { this.state.ninjas } />
-              <AddNinja/>
+              <AddNinja addNinja = { this.addNinja } />
     </div>
   );
 }
