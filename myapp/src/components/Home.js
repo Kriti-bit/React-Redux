5dfe9c5669ea08a69476d4421725fc0fbb42d15f
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-13 07:02:52 +0530

    #40 Starting Redux

--- myapp/src/components/Home.js
+++ myapp/src/components/Home.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import '../index.css'
+//import axios from 'axios'
 import { Link } from 'react-router-dom'
 import pokeball from '../pokeball.png'
+import { connect } from 'react-redux'
+
 class Home extends Component {
-    state = {
-        post: [ ]
-    }
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts/')
-         .then(res => {
-             console.log(res)
-             this.setState({
-                 post: res.data.slice(0,10)
-             })
-         })
-    }
+    // state = {
+    //     post: [ ]
+    // }
+    // componentDidMount(){
+    //     axios.get('https://jsonplaceholder.typicode.com/posts/')
+    //      .then(res => {
+    //          console.log(res)
+    //          this.setState({
+    //              post: res.data.slice(0,10)
+    //          })
+    //      })
+    // }
 
     render(){
-        const { post } = this.state;
-        const postList = post.length ? (
-            post.map( post => {
+        console.log(this.props);
+        //const { post } = this.state;
+        const { posts } = this.props
+        const postList = posts.length ? (
+            posts.map( posts => {
                 return(
-                    <div className = "post card" key={post.id}>
+                    <div className = "post card" key={posts.id}>
                     <img src={ pokeball } alt = "Poke Ball Here"/>
                         <div className="card-content">
-                          <Link to = {'/' + post.id }> 
-                            <span className="card-title red-text">{ post.title } </span>
+                          <Link to = {'/' + posts.id }> 
+                            <span className="card-title red-text">{ posts.title } </span>
                           </Link>
-                            <p>{post.body}</p>
+                            <p>{posts.body}</p>
                         </div>
                     </div>
                 )
@@ -36,7 +41,7 @@ class Home extends Component {
             <div className="center"> No post yet </div>
         )
         return (
-            <div className="container">
+            <div className="container home">
                 <h4 className="center">Home</h4>
                 {postList}
             </div>
@@ -44,4 +49,10 @@ class Home extends Component {
     }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        posts : state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home)
